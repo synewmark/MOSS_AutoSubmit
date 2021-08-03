b368264575ca2270093acbd2248f339e9c8ad5c3
Shmuel Newmark <snewmark@mail.yu.edu>
2021-08-03 16:03:43 -0400

    reworked git_downloader static methods to use builder internally

--- src/git_handler/Git_downloader_svn.java
+++ src/git_handler/Git_downloader_svn.java
@@ -5,122 +5,96 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.SVNURL;
+import org.tmatesoft.svn.core.wc.SVNWCUtil;
 import org.tmatesoft.svn.core.wc2.SvnCheckout;
 import org.tmatesoft.svn.core.wc2.SvnOperationFactory;
 import org.tmatesoft.svn.core.wc2.SvnTarget;
-import org.tmatesoft.svn.core.SVNURL;
-import org.tmatesoft.svn.core.wc.SVNWCUtil;
-
 
 public class Git_downloader_svn {
-	File directory;
-	SvnOperationFactory svnOperationFactory = new SvnOperationFactory();
-    SvnCheckout checkout = svnOperationFactory.createCheckout();
-	
+	// instantiate directory file on Object to perform cleanup
+	private File directory;
+	private final SvnOperationFactory svnOperationFactory = new SvnOperationFactory();
+	private final SvnCheckout checkout = svnOperationFactory.createCheckout();
+
 	public Git_downloader_svn setCredentials(char[] oauthToken) {
-		svnOperationFactory.setAuthenticationManager(SVNWCUtil.createDefaultAuthenticationManager("${token}", oauthToken));
+		// despite potential appearances, SvnCheckout does remain linked to the relevant
+		// instance of SvnOperationFactory
+		// setAuthenticationManager can be called after createCheckout without issue
+		svnOperationFactory
+				.setAuthenticationManager(SVNWCUtil.createDefaultAuthenticationManager("${token}", oauthToken));
 		return this;
 	}
+
 	public Git_downloader_svn setDownloadSource(String urlToDownload) {
 		try {
 			checkout.setSource(SvnTarget.fromURL(SVNURL.parseURIEncoded(gitURLtoSVN(urlToDownload))));
 		} catch (SVNException e) {
-			throw new IllegalArgumentException("URL "+ urlToDownload+" is malformed!", e);
+			throw new IllegalArgumentException("URL " + urlToDownload + " is malformed!", e);
 		}
 		return this;
 	}
-	
+
 	public Git_downloader_svn setOutputFolder(File directory) {
+		if (!directory.mkdirs() || !directory.canWrite()) {
+			throw new IllegalArgumentException("Cannot write to " + directory);
+		}
 		checkout.setSingleTarget(SvnTarget.fromFile(directory));
 		this.directory = directory;
 		return this;
 	}
-	
+
 	public void execute() {
 		try {
 			checkout.run();
-			delete(new File(directory.toString()+File.separatorChar+".svn"));
+			delete(new File(directory.toString() + File.separatorChar + ".svn"));
 		} catch (SVNException e) {
+			// TODO: instance check exceptions and rewrap solvable ones into other
+			// exceptions with more detailed error messages
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void downloadGitRepo(String urlToDownload, File directory) {
-		SvnOperationFactory svnOperationFactory = new SvnOperationFactory();
-		try {
-		    SvnCheckout checkout = svnOperationFactory.createCheckout();
-		    checkout.setSource(SvnTarget.fromURL(SVNURL.parseURIEncoded(gitURLtoSVN(urlToDownload))));
-		    checkout.setSingleTarget(SvnTarget.fromFile(directory));
-		    checkout.run();
-		    delete(new File(directory.toString()+File.separatorChar+".svn"));
-		} catch (SVNException e) {
-			e.printStackTrace();
-		} finally {
-		    svnOperationFactory.dispose();
-		}
+		new Git_downloader_svn().setOutputFolder(directory).setDownloadSource(urlToDownload).execute();
 	}
 
 	public static File downloadGitRepo(String urlToDownload) {
-		SvnOperationFactory svnOperationFactory = new SvnOperationFactory();
 		File tempDirectory = null;
 		try {
-			tempDirectory = Files.createTempDirectory("random").toFile();
-		    SvnCheckout checkout = svnOperationFactory.createCheckout();
-		    checkout.setSource(SvnTarget.fromURL(SVNURL.parseURIEncoded(gitURLtoSVN(urlToDownload))));
-		    checkout.setSingleTarget(SvnTarget.fromFile(tempDirectory));
-		    checkout.run();
-		    delete(new File(tempDirectory.toString()+File.separatorChar+".svn"));
-		} catch (SVNException e) {
-			e.printStackTrace();
+			tempDirectory = Files.createTempDirectory("").toFile();
 		} catch (IOException e) {
 			e.printStackTrace();
-		} finally {
-		    svnOperationFactory.dispose();
 		}
+		new Git_downloader_svn().setOutputFolder(tempDirectory).setDownloadSource(urlToDownload).execute();
 		return tempDirectory;
 	}
-	
+
 	public static void downloadGitRepo(String urlToDownload, File directory, char[] oauthToken) {
-		SvnOperationFactory svnOperationFactory = new SvnOperationFactory();
-		svnOperationFactory.setAuthenticationManager(SVNWCUtil.createDefaultAuthenticationManager("${token}", oauthToken));
-		try {
-			SvnCheckout checkout = svnOperationFactory.createCheckout();
-		    checkout.setSource(SvnTarget.fromURL(SVNURL.parseURIEncoded(gitURLtoSVN(urlToDownload))));
-		    checkout.setSingleTarget(SvnTarget.fromFile(directory));
-		    checkout.run();
-		    delete(new File(directory.toString()+File.separatorChar+".svn"));
-		} catch (SVNException e) {
-			e.printStackTrace();
-		} finally {
-		    svnOperationFactory.dispose();
-		}
+		new Git_downloader_svn().setCredentials(oauthToken).setOutputFolder(directory).setDownloadSource(urlToDownload)
+				.execute();
 	}
 
 	public static File downloadGitRepo(String urlToDownload, char[] oauthToken) {
-		SvnOperationFactory svnOperationFactory = new SvnOperationFactory();
-		svnOperationFactory.setAuthenticationManager(SVNWCUtil.createDefaultAuthenticationManager("${token}", oauthToken));
 		File tempDirectory = null;
 		try {
-			tempDirectory = Files.createTempDirectory("random").toFile();
-		    SvnCheckout checkout = svnOperationFactory.createCheckout();
-		    checkout.setSource(SvnTarget.fromURL(SVNURL.parseURIEncoded(gitURLtoSVN(urlToDownload))));
-		    checkout.setSingleTarget(SvnTarget.fromFile(tempDirectory));
-		    checkout.run();
-		    delete(new File(tempDirectory.toString()+File.separatorChar+".svn"));
-		} catch (SVNException e) {
-			e.printStackTrace();
+			tempDirectory = Files.createTempDirectory("").toFile();
 		} catch (IOException e) {
 			e.printStackTrace();
-		} finally {
-		    svnOperationFactory.dispose();
 		}
+		new Git_downloader_svn().setCredentials(oauthToken).setOutputFolder(tempDirectory)
+				.setDownloadSource(urlToDownload).execute();
 		return tempDirectory;
 	}
-	
+
 	private static String gitURLtoSVN(String url) {
+		// pattern matches any substring starting with "tree/" until and including the
+		// next '/' and replaces it with "trunk"
+		// see
+		// https://stackoverflow.com/questions/7106012/download-a-single-folder-or-directory-from-a-github-repo
 		return url.replaceFirst("tree/.*/", "trunk/");
 	}
-	
+
 	private static void delete(File file) {
 		if (file.isDirectory()) {
 			for (File f : file.listFiles()) {
--- test/git_handler/Git_test.java
+++ test/git_handler/Git_test.java
@@ -7,20 +7,23 @@ import java.io.File;
 import org.junit.jupiter.api.Test;
 
 public class Git_test {
-	File testDirectory = new File(System.getProperty("user.dir")+"\\testDownload");
+	File testDirectory = new File(System.getProperty("user.dir") + "\\testDownload");
+
 	@Test
 	public void testGitDownload() {
 		Git_downloader_svn.downloadGitRepo("https://github.com/google/gson/tree/master/lib", testDirectory);
 		assertEquals(testDirectory.listFiles().length, 2);
 		cleanup(testDirectory);
 	}
+
 	@Test
 	public void testGitDownloadBuilder() {
-		new Git_downloader_svn().setOutputFolder(testDirectory).
-		setDownloadSource("https://github.com/google/gson/tree/master/lib").execute();
+		new Git_downloader_svn().setOutputFolder(testDirectory)
+				.setDownloadSource("https://github.com/google/gson/tree/master/lib").execute();
 		assertEquals(testDirectory.listFiles().length, 2);
 		cleanup(testDirectory);
 	}
+
 	public void cleanup(File file) {
 		if (file.isDirectory()) {
 			for (File f : file.listFiles()) {
