762875bb2ddac1d7be6466c3d6a014c55e05fdad
synewmark <snewmark@mail.yu.edu>
2021-11-03 13:05:23 -0400

    Made order of code execution not dependent on cli order

--- src/runner/MainCommandLine.java
+++ src/runner/MainCommandLine.java
@@ -6,45 +6,68 @@ import java.util.Scanner;
 
 public class MainCommandLine {
 	static Scanner scanner = new Scanner(System.in);
+	static String[] gitRequest;
+	static String[] mossRequest;
 
 	public static void main(String[] args) {
 		String[][] parsedArgs = parser(args);
+		setFields(parsedArgs);
+
+		if (gitRequest != null) {
+			gitRequest();
+		}
+		if (mossRequest != null) {
+			mossRequest();
+		}
+
+	}
+
+	private static void setFields(String[][] parsedArgs) {
 		for (String[] stringArray : parsedArgs) {
 			switch (stringArray[0].toLowerCase()) {
 
 			case "--m":
-				System.out.println("Executing MOSS request...");
-				System.out.println();
-				String resultsURL;
-				if (stringArray.length > 1) {
-					resultsURL = new MOSSRunner(Arrays.copyOfRange(stringArray, 1, stringArray.length)).execute();
-				} else {
-					resultsURL = new MOSSRunner().execute();
-				}
-				System.out.println();
-				System.out.println("MOSS request completed");
-				System.out.println("You can view your results here: " + resultsURL);
-				System.out.println();
+				mossRequest = Arrays.copyOfRange(stringArray, 1, stringArray.length);
 				break;
 
 			case "--g":
-				System.out.println("Executing Git request...");
-				System.out.println();
-				File downloadLocation;
-				if (stringArray.length > 1) {
-					downloadLocation = new GitRunner(Arrays.copyOfRange(stringArray, 1, stringArray.length)).execute();
-				} else {
-					downloadLocation = new GitRunner().execute();
-				}
-				System.out.println("Git download completed you can find your files at this directory: ");
-				System.out.println(downloadLocation);
-				System.out.println();
+				gitRequest = Arrays.copyOfRange(stringArray, 1, stringArray.length);
 				break;
 
 			default:
-				throw new IllegalStateException(stringArray[0] + " is not a recognized command");
+				throw new IllegalArgumentException(stringArray[0] + " is not a recognized command");
 			}
 		}
+	}
+
+	private static void gitRequest() {
+		System.out.println("Executing Git request...");
+		System.out.println();
+		File downloadLocation;
+		if (gitRequest.length > 0) {
+			downloadLocation = new GitRunner(gitRequest).execute();
+		} else {
+			downloadLocation = new GitRunner().execute();
+		}
+		System.out.println("Git download completed you can find your files at this directory: ");
+		System.out.println(downloadLocation);
+		System.out.println();
+	}
+
+	private static void mossRequest() {
+		System.out.println("Executing MOSS request...");
+		System.out.println();
+		String resultsURL;
+		if (mossRequest.length > 0) {
+			resultsURL = new MOSSRunner(mossRequest).execute();
+		} else {
+			resultsURL = new MOSSRunner().execute();
+		}
+		System.out.println();
+		System.out.println("MOSS request completed");
+		System.out.println("You can view your results here: " + resultsURL);
+		System.out.println();
+
 	}
 
 	private static String[][] parser(String[] inputs) {
