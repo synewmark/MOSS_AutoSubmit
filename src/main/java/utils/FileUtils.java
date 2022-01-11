1ecfb134bddf82c3a83415aba86dd4f0a48b21a8
Shmuel Newmark <snewmark@mail.yu.edu>
2022-01-10 22:35:54 -0500

    Added some additional utils needed by codequiry

--- src/main/java/utils/FileUtils.java
+++ src/main/java/utils/FileUtils.java
@@ -2,7 +2,18 @@ package utils;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class FileUtils {
 
@@ -37,6 +48,27 @@ public class FileUtils {
 		return fileToDelete.delete();
 	}
 
+	// returns first overlapped directory or null if all are disjoint
+	public static List<File> checkOverlappingDirectories(Collection<File> files) {
+		Map<File, File> fileComponents = new HashMap<>();
+		for (File file : files) {
+			File parent = file.getParentFile();
+			while (parent != null) {
+				fileComponents.put(parent, file);
+				parent = parent.getParentFile();
+			}
+		}
+		for (File file : files) {
+			if (fileComponents.containsKey(file)) {
+				List<File> returnList = new ArrayList<>();
+				returnList.add(file);
+				returnList.add(fileComponents.get(file));
+				return returnList;
+			}
+		}
+		return null;
+	}
+
 	// checks if we have write access to the directory or if the directory doesn't
 	// exist if we have write access to the parent directory
 	// in slightly different terms: returns true if we have write access to passed
@@ -51,5 +83,46 @@ public class FileUtils {
 			return checkWriteAccessOfDir(dir.getParentFile());
 		}
 		return dir.canWrite();
+	}
+
+	public static File zipDirectory(File directoryToZip, File zipFileName) throws IOException {
+		if (!directoryToZip.isDirectory()) {
+			throw new IllegalArgumentException(directoryToZip + " is not a directory");
+		}
+		if (!directoryToZip.canRead()) {
+			throw new IllegalArgumentException("Cannot read from: " + directoryToZip
+					+ " make sure the directory is named correctly and that you have the required permissions");
+		}
+		zipFileName.getParentFile().mkdirs();
+		// use Try despite throw in method to ensure closing of resources
+		try (FileOutputStream fileOutput = new FileOutputStream(zipFileName);
+				ZipOutputStream zipOutput = new ZipOutputStream(fileOutput);) {
+			Queue<File> filesToZip = new ArrayDeque<>();
+			filesToZip.add(directoryToZip);
+			while (!filesToZip.isEmpty()) {
+				File currFile = filesToZip.poll();
+				if (currFile.isDirectory()) {
+					for (File file : currFile.listFiles()) {
+						filesToZip.add(file);
+					}
+				} else if (currFile.isFile()) {
+					zipOutput.putNextEntry(new ZipEntry(getRelativeFile(directoryToZip, currFile).toString()));
+					Files.copy(currFile.toPath(), zipOutput);
+				}
+			}
+		}
+		return zipFileName;
+
+	}
+
+	private static File getRelativeFile(File parent, File child) {
+		return parent.toPath().relativize(child.toPath()).toFile();
+	}
+
+	public static String getFileName(File file) {
+		return file.toPath().getFileName().toString();
+	}
+
+	private FileUtils() {
 	}
 }
