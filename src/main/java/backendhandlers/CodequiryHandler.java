38c902dfabad4174a647f90022c643d59b9db5d1
Shmuel Newmark <snewmark@mail.yu.edu>
2022-01-10 22:35:27 -0500

    Changed maven dependencies to use my codequiry api fork

---
+++ src/main/java/backendhandlers/CodequiryHandler.java
@@ -0,0 +1,118 @@
+package backendhandlers;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import src.CodequirySDK;
+import src.model.Check;
+import src.model.CheckStatus;
+import utils.FileUtils;
+
+public class CodequiryHandler {
+
+	// for some baffling reason the createCheck endpoint doesn't actually accept a
+	// language string like the API docs suggest
+	// instead it accepts an int which corresponds to a specific language
+	// the map translates the language name to the correct integer
+	private static final Map<String, Integer> langaugeMap = getPopulatedLanguageMap();
+
+	private String apiKey;
+	private String checkName;
+	private Integer language;
+	private SortedSet<File> setOfDirectoriesToSubmit = new TreeSet<>();
+
+	public CodequiryHandler setLanguage(String language) {
+		language = language.toLowerCase();
+		if (!langaugeMap.containsKey(language)) {
+			throw new IllegalArgumentException("Langauge unsupported");
+		}
+		this.language = langaugeMap.get(language);
+		return this;
+	}
+
+	public CodequiryHandler setAPIKey(String apiKey) {
+		this.apiKey = apiKey;
+		return this;
+	}
+
+	public CodequiryHandler setSubmissionName(String checkName) {
+		this.checkName = checkName;
+		return this;
+	}
+
+	public CodequiryHandler addDirectoryToSubmit(File directoryToSubmit) {
+		setOfDirectoriesToSubmit.add(directoryToSubmit);
+		return this;
+	}
+
+	public URL execute() throws IOException {
+		prechecks();
+		CodequirySDK api = new CodequirySDK(apiKey);
+		Check check = api.createCheck(checkName, Integer.toString(language));
+		SortedSet<File> zipFilesToUpload = new TreeSet<>();
+		int checkId = check.getId();
+		for (File directoryToSubmit : setOfDirectoriesToSubmit) {
+			File fileToUpload = FileUtils.zipDirectory(directoryToSubmit,
+					new File(directoryToSubmit.getParent(), String.format("%sZip_FileCount%d.zip",
+							FileUtils.getFileName(directoryToSubmit), FileUtils.getNumberOfFiles(directoryToSubmit))));
+			zipFilesToUpload.add(fileToUpload);
+		}
+		for (File uploadFile : zipFilesToUpload) {
+			api.upload(check.getId(), uploadFile.toString());
+		}
+
+		CheckStatus checkStatus = api.startCheck(checkId);
+		try {
+			return new URL(checkStatus.getCheckURL());
+		} catch (MalformedURLException e) {
+			throw new IOException("Invalid response from the server", e);
+		}
+
+	}
+
+	private void prechecks() {
+		if (apiKey == null) {
+			throw new IllegalArgumentException("API Key not set");
+		}
+
+		if (checkName == null) {
+			throw new IllegalArgumentException("Check name not set");
+		}
+
+		if (language == null) {
+			throw new IllegalArgumentException("Language not set");
+		}
+
+		if (setOfDirectoriesToSubmit.isEmpty()) {
+			throw new IllegalArgumentException("Directories empty");
+		}
+
+		List<File> overlap;
+		if ((overlap = FileUtils.checkOverlappingDirectories(setOfDirectoriesToSubmit)) != null) {
+			throw new IllegalArgumentException(
+					String.format("Directory A: %s Overlaps directory B: %s", overlap.get(0), overlap.get(1)));
+		}
+	}
+
+	private static Map<String, Integer> getPopulatedLanguageMap() {
+		// todo: finish manually populating map
+		final HashMap<String, Integer> map = new HashMap<>();
+		map.put("java", 13);
+		map.put("python", 14);
+		map.put("c", 17);
+		map.put("c++", 17);
+		map.put("c#", 18);
+		map.put("perl", 20);
+		map.put("php", 21);
+		map.put("sql", 22);
+		return Collections.unmodifiableMap(map);
+	}
+}
