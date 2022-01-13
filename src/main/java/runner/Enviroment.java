c150efed4a91e361c1bd50e8acce96a4045d5f6f
Shmuel Newmark <snewmark@mail.yu.edu>
2022-01-12 23:38:24 -0500

    Environment now tracks list of student directories and root

--- src/main/java/runner/Enviroment.java
+++ src/main/java/runner/Enviroment.java
@@ -1,10 +1,16 @@
 package runner;
 
 import java.io.File;
+import java.util.Collections;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 public class Enviroment {
 	private static String workingLanguage;
-	private static File workingStudentFileDir;
+	private static File rootWorkingStudentFileDir;
+	private static SortedSet<File> studentDirectories = new TreeSet<>();
+	private static Set<File> unmodifiableStudentDirectories = Collections.unmodifiableSet(studentDirectories);
 
 	public static String getWorkingLanguage() {
 		return workingLanguage;
@@ -14,12 +20,20 @@ public class Enviroment {
 		Enviroment.workingLanguage = workingLanguage;
 	}
 
-	public static File getWorkingStudentFileDir() {
-		return workingStudentFileDir;
+	public static File getRootWorkingStudentFileDir() {
+		return rootWorkingStudentFileDir;
 	}
 
-	public static void setWorkingStudentFileDir(File workingStudentFileDir) {
-		Enviroment.workingStudentFileDir = workingStudentFileDir;
+	public static void setRootWorkingStudentFileDir(File rootWorkingStudentFileDir) {
+		Enviroment.rootWorkingStudentFileDir = rootWorkingStudentFileDir;
+	}
+
+	public static boolean addStudentDirectory(File directory) {
+		return studentDirectories.add(directory);
+	}
+
+	public static Set<File> getStudentDirectories() {
+		return unmodifiableStudentDirectories;
 	}
 
 	private Enviroment() {
