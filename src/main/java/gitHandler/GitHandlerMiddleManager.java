182567c0613faf9b254b54aee408b3df2fe26c76
Shmuel Newmark <snewmark@mail.yu.edu>
2022-01-18 02:31:43 -0500

    RegEx Or is | not ||

--- src/main/java/gitHandler/GitHandlerMiddleManager.java
+++ src/main/java/gitHandler/GitHandlerMiddleManager.java
@@ -53,7 +53,7 @@ public class GitHandlerMiddleManager extends GitHandlerAbstract {
 			Set<File> set = new HashSet<>((int) (explicitFilesToDownload.size() * 0.75) + 1);
 			for (String specificFile : explicitFilesToDownload) {
 				File relative = new File(urlToDownload.getPath(), specificFile);
-				File fileToAdd = new File(directoryToRepo, relative.toString().replaceAll("\\||/", File.separator));
+				File fileToAdd = new File(directoryToRepo, relative.toString().replaceAll("\\|/", File.separator));
 				set.add(fileToAdd);
 			}
 			FileFilter filter = (File file) -> !set.contains(file);
