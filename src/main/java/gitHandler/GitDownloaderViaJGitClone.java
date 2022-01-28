e40471289aa21cabcdbad5f354e080b1f4abfb9b
Shmuel Newmark <snewmark@mail.yu.edu>
2022-01-28 00:32:55 -0500

    Fixed timestamp bug

--- src/main/java/gitHandler/GitDownloaderViaJGitClone.java
+++ src/main/java/gitHandler/GitDownloaderViaJGitClone.java
@@ -3,6 +3,7 @@ package gitHandler;
 import java.io.File;
 import java.io.IOException;
 
+import org.eclipse.jgit.api.CheckoutCommand;
 import org.eclipse.jgit.api.CloneCommand;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
@@ -14,22 +15,25 @@ public class GitDownloaderViaJGitClone extends GitHandlerAbstract {
 
 	@Override
 	public void execute() throws IOException {
-		String hash = null;
-		if (this.dateToDownload != null) {
-			hash = GitDownloaderViaAPIRequests.getGitCommitForDate(urlToDownload, dateToDownload, oauthToken);
-		}
-		this.directoryToDownloadTo = new File(this.directoryToDownloadTo,
-				this.urlToDownload.getUsername() + File.separator + this.urlToDownload.getRepoName());
+		this.directoryToDownloadTo = new File(this.directoryToDownloadTo, this.urlToDownload.getRepoName());
 
 		CloneCommand clone = new CloneCommand().setDirectory(directoryToDownloadTo)
 				.setURI(this.urlToDownload.getBareURL().toString());
 
-		clone.setBranch(hash != null ? hash : urlToDownload.getBranch());
+		clone.setBranch(urlToDownload.getBranch());
 
 		if (this.oauthToken != null) {
 			clone.setCredentialsProvider(new UsernamePasswordCredentialsProvider("${token}", oauthToken));
 		}
 		try (Git git = clone.call();) {
+			// this or revwalk...
+			if (dateToDownload != null) {
+				String hash = GitDownloaderViaAPIRequests.getGitCommitForDate(urlToDownload, dateToDownload,
+						oauthToken);
+				CheckoutCommand checkout = git.checkout();
+				checkout.setName(hash);
+				checkout.call();
+			}
 		} catch (GitAPIException e) {
 			throw new IOException(e);
 		}
