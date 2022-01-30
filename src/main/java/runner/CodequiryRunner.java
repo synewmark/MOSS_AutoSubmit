885ba5112f76a0fd7998483973020b4c341e48ce
Shmuel Newmark <snewmark@mail.yu.edu>
2022-01-30 17:48:12 -0500

    Adapted to code for use with Google Options instead of manual Switch
    Also removed interactive mode

--- src/main/java/runner/CodequiryRunner.java
+++ src/main/java/runner/CodequiryRunner.java
@@ -3,211 +3,65 @@ package runner;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.util.Collection;
 import java.util.Date;
-import java.util.List;
-import java.util.Scanner;
-import java.util.Set;
-import java.util.TreeSet;
 
 import backendhandlers.CodequiryHandler;
-import utils.FileUtils;
 
 public class CodequiryRunner {
-	private static final Scanner scanner = new Scanner(System.in);
 	private String apiKey;
 	private String language;
-	private Collection<File> directoriesToUpload;
+	private File directoriesToUpload;
 	private String name;
 
-	public CodequiryRunner(String[] params) {
-		setFields(params);
-		getUnsetFields(false);
+	public CodequiryRunner(Enviroment eviroment) {
+		vertifyAndPassParameters(eviroment);
 	}
 
-	public CodequiryRunner() {
-		getUnsetFields(true);
+	private void vertifyAndPassParameters(Enviroment enviroment) {
+		this.apiKey = enviroment.codequiryAPI;
+		this.directoriesToUpload = enviroment.directory;
+		this.language = enviroment.language;
+		this.name = enviroment.codequiryRequestName;
+		checkNullValues();
 	}
 
-	private void getUnsetFields(boolean getOptionalFields) {
-		if (apiKey == null) {
-			apiKey = getApiKey();
-		}
-
-		if (language == null) {
-			language = getLanguage();
+	private void checkNullValues() {
+		if (apiKey == null || apiKey.isBlank()) {
+			throw new IllegalStateException("Codequiry API not set");
 		}
 
 		if (directoriesToUpload == null) {
-			directoriesToUpload = getDirectoriesToUpload();
+			throw new IllegalStateException("Student Directory not set");
 		}
-		if (getOptionalFields) {
-			if (name == null) {
-				name = getName();
-			}
+
+		if (!directoriesToUpload.canRead()) {
+			throw new IllegalStateException("Cannot read from Student directory: " + directoriesToUpload);
+		}
+
+		if (!directoriesToUpload.isDirectory() || directoriesToUpload.list().length == 0) {
+			throw new IllegalStateException("Student directory is empty or a not a valid directory");
+		}
+
+		if (language == null) {
+			throw new IllegalStateException("Language not set");
 		}
 	}
 
 	public URL execute() {
+		File[] filesToUpload = directoriesToUpload.listFiles();
 		if (name == null) {
 			name = String.format("AutosubmitRequest: %s StudentCount: %d",
-					new Date(System.currentTimeMillis()).toString(), directoriesToUpload.size());
+					new Date(System.currentTimeMillis()).toString(), filesToUpload.length);
 		}
 		CodequiryHandler codequiryHandler = new CodequiryHandler();
 		codequiryHandler.setAPIKey(apiKey).setLanguage(language).setSubmissionName(name);
-		for (File file : directoriesToUpload) {
+		for (File file : filesToUpload) {
 			codequiryHandler.addDirectoryToSubmit(file);
 		}
 		try {
 			return codequiryHandler.execute();
 		} catch (IOException e) {
 			throw new IllegalStateException(e);
-		}
-	}
-
-	private static String getLanguage() {
-		if (Enviroment.getWorkingLanguage() != null) {
-			System.out.println("Found working language: " + Enviroment.getWorkingLanguage());
-			System.out.println("If you would like to use it enter Y");
-			String response = scanner.nextLine();
-			if (response.equals("Y")) {
-				return Enviroment.getWorkingLanguage();
-			}
-		}
-		System.out.println("Please enter the language the code is in");
-		String response = scanner.nextLine();
-		return response;
-	}
-
-	private static String getApiKey() {
-		System.out.println("Please enter the API key for Codeqiry");
-		String response = scanner.nextLine();
-		if (response.isBlank()) {
-			System.out.println("Invalid API key, please try again");
-			return getApiKey();
-		}
-		return response;
-	}
-
-	private static String getName() {
-		System.out.println("Please enter a name for your Codeqiry request");
-		System.out.println("You can also leave this blank and one will be assigned to you");
-		String response = scanner.nextLine();
-		if (response.isBlank()) {
-			return null;
-		}
-		return response;
-
-	}
-
-	private static Collection<File> getDirectoriesToUpload() {
-		if (!Enviroment.getStudentDirectories().isEmpty()) {
-			System.out.println(String.format("Found %d working student directories from previous Git request",
-					Enviroment.getStudentDirectories().size()));
-			System.out.println(
-					"If you would like to use those enter Y, to review them enter P, otherwise enter any other key");
-			while (true) {
-				String response = scanner.nextLine();
-				if (response.equals("Y")) {
-					return Enviroment.getStudentDirectories();
-				} else if (!response.equals("P")) {
-					break;
-				} else {
-					int i = 1;
-					for (File file : Enviroment.getStudentDirectories()) {
-						System.out.println("Directory " + i++ + ": " + file);
-					}
-				}
-			}
-		}
-		boolean directory;
-		System.out.println(
-				"Please enter whether the directory about to be passed is to a root directory or a text list of directories");
-		while (true) {
-			System.out.println("Please enter \"root\" if it's a root and \"txt\" if it's a text file");
-			String reponse = scanner.nextLine();
-			if (reponse.equals("root")) {
-				directory = true;
-				break;
-			} else if (reponse.equals("txt")) {
-				directory = false;
-				break;
-			}
-		}
-		System.out.println("Now please enter the path");
-		String reponse = scanner.nextLine();
-		File file = new File(reponse);
-		if (!file.canRead()) {
-			System.out.println("Cannot read from file: " + reponse);
-			System.out.println("Please try again");
-			return getDirectoriesToUpload();
-		}
-		if (directory) {
-			return new TreeSet<File>(List.of(file.listFiles()));
-
-		}
-		Collection<File> returnCollection = new TreeSet<>();
-		Collection<String> listOfFiles;
-		try {
-			listOfFiles = FileUtils.getListOfStringsFromFile(file);
-		} catch (IOException e) {
-			throw new IllegalStateException("Could not read from file: " + file, e);
-		}
-		for (String string : listOfFiles) {
-			returnCollection.add(new File(string));
-		}
-		return returnCollection;
-	}
-
-	private void setFields(String[] params) {
-		for (int i = 0; i < params.length; i++) {
-			switch (params[i].toLowerCase()) {
-			case "-name":
-			case "-n":
-				name = params[++i];
-			case "-language":
-			case "-l":
-				language = params[++i];
-				break;
-			case "-key":
-			case "-apikey":
-				apiKey = params[++i];
-				break;
-			case "-sd":
-			case "studentdirectory":
-				File rootDirectory = new File(params[++i]);
-				directoriesToUpload = new TreeSet<File>(List.of(rootDirectory.listFiles()));
-				break;
-
-			case "-sdf":
-			case "-studentdirectoriesfile":
-				File fileDirectoriesToUpload = new File(params[++i]);
-				Set<File> set = new TreeSet<>();
-				Collection<String> listOfFiles;
-				try {
-					listOfFiles = FileUtils.getListOfStringsFromFile(fileDirectoriesToUpload);
-				} catch (IOException e) {
-					throw new IllegalStateException("Could not read from file: " + fileDirectoriesToUpload, e);
-				}
-				for (String string : listOfFiles) {
-					set.add(new File(string));
-				}
-				directoriesToUpload = set;
-			default:
-				throw new IllegalArgumentException(params[i] + " is not a valid flag for Codequiry Upload");
-			}
-		}
-		String cacheLanguage = Enviroment.getWorkingLanguage();
-		if (language == null && cacheLanguage != null) {
-			System.out.println("Language not supplied. Using working language: " + cacheLanguage);
-			System.out.println();
-			language = cacheLanguage;
-		}
-
-		Collection<File> cacheSetOfDirectories = Enviroment.getStudentDirectories();
-		if (directoriesToUpload == null && cacheSetOfDirectories.size() != 0) {
-			System.out.println("Student File Directory not supplied. Using working set: " + cacheSetOfDirectories);
-			directoriesToUpload = cacheSetOfDirectories;
 		}
 	}
 }
--- src/main/java/runner/GitRunner.java
+++ src/main/java/runner/GitRunner.java
@@ -1,13 +1,10 @@
 package runner;
 
-import java.io.Console;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeParseException;
 import java.util.List;
-import java.util.Scanner;
 
 import gitHandler.GitHandlerAbstract;
 import gitHandler.GitHandlerMiddleManager;
@@ -15,8 +12,7 @@ import gitHandler.model.URLbyComponents;
 import utils.FileUtils;
 
 public class GitRunner {
-	private static final Scanner scanner = new Scanner(System.in);
-	private String host = "https://github.com/";
+
 	private String username;
 	private File pathToRepoNameFile;
 	private String subdirectory = "";
@@ -26,249 +22,49 @@ public class GitRunner {
 	private LocalDateTime timestamp = LocalDateTime.now();
 	private File pathToListOfFilesToDownload;
 
-	GitRunner(String[] params) {
-		setFields(params);
-		setUnsetFields(false);
+	private static final String gitHost = "https://github.com/";
+
+	public GitRunner(Enviroment enviroment) {
+		vertifyAndPassParameters(enviroment);
 	}
 
-	GitRunner() {
-		setUnsetFields(true);
+	private void vertifyAndPassParameters(Enviroment enviroment) {
+		this.oauthToken = enviroment.gitAPI;
+		this.username = enviroment.username;
+		this.pathToRepoNameFile = enviroment.repoPath;
+		this.branch = enviroment.branch;
+		this.subdirectory = enviroment.subdirectory;
+		this.pathToListOfFilesToDownload = enviroment.filesToDownload;
+		this.timestamp = enviroment.timestamp;
+		checkNullValues();
 	}
 
-	private static String getHost() {
-		System.out.println("Please enter the base Git URL to the files (default: https://github.com/) :");
-		String stringURL = scanner.nextLine();
-		if (stringURL.equals("")) {
-			stringURL = "https://github.com/";
-		}
-		return stringURL;
-	}
+	private void checkNullValues() {
 
-	private static String getUsername() {
-		System.out.println("Please enter the username hosting the repositories:");
-		String username = scanner.nextLine();
-		return username;
-	}
-
-	private static File getPathToRepoNameFile() {
-		System.out.println("Please enter the directory to a text document hosting the list of repos :");
-		String fileOfRepos = scanner.nextLine();
-		File file = new File(fileOfRepos);
-		if (!file.exists() || !file.canRead()) {
-			System.out.println("Cannot access the file!");
-			System.out.println("Please try again");
-			return getPathToRepoNameFile();
-		}
-		return file;
-	}
-
-	private static String getSubdirectory() {
-		System.out.println("Please enter the subdirectory you wish to download of the git repo: ");
-		System.out.println("You may also leave this field blank to download the entire repository");
-		String directory = scanner.nextLine();
-		return directory;
-	}
-
-	private static String getBranch() {
-		System.out.println("Please enter the branch of the Git Repository you wish to download: ");
-		String branch = scanner.nextLine();
-		return branch;
-	}
-
-	private static char[] getOauthToken() {
-		System.out.println("If this repo is private please enter an OAuth token with valid read permission :");
-		System.out.println("You may also wish to enter a token to avoid API limitting");
-		char[] oauthToken = null;
-		Console console = System.console();
-		if (console != null) {
-			oauthToken = console.readPassword();
-		} else {
-			oauthToken = scanner.nextLine().toCharArray();
-		}
-		if (oauthToken.length == 0) {
-			return null;
-		}
-		return oauthToken;
-	}
-
-	private static File getDirectoryToDownloadTo() {
-		System.out.println("Please enter the directory in which you'd like these files to be downloaded");
-		System.out.println("You can also leave this field blank and a temp directory will be assigned to you");
-		String stringDirectory = scanner.nextLine();
-		File fileDirectory = null;
-		if (!stringDirectory.isBlank()) {
-			fileDirectory = new File(stringDirectory);
-		} else {
-			try {
-				fileDirectory = Files.createTempDirectory("MOSSAutoSubmit_GitFiles").toFile();
-			} catch (IOException e) {
-				System.out.println(
-						"Cannot create temp directory for you, please specificy a valid directory and try again");
-			}
-		}
-		if (!FileUtils.checkWriteAccessOfDir(fileDirectory)) {
-			System.out.println("Cannot write to " + fileDirectory.toString() + " please try again");
-			return getDirectoryToDownloadTo();
-		}
-		return fileDirectory;
-	}
-
-	private static LocalDateTime getTimeStamp() {
-		System.out.println(
-				"Please enter the ISO-8061 compliant time stamp corresponding to the latest date you wish to be downloaded");
-		System.out.println("You can also leave this field blank and the latest repository will be downloaded");
-		LocalDateTime timestamp = null;
-		String passedValue = scanner.nextLine();
-		if (passedValue.equals("")) {
-			timestamp = LocalDateTime.MAX;
-		} else {
-			try {
-				timestamp = LocalDateTime.parse(passedValue);
-			} catch (DateTimeParseException e) {
-				System.out.println(
-						"Time stamp invalid, please enter an ISO-8061 compliant time stamp (YYYY-MM-DDTHH:MM:SSZ)");
-				return getTimeStamp();
-			}
-		}
-		return timestamp;
-	}
-
-	private static File getPathToListOfFilesToDownload() {
-		System.out.println("Please enter the path to the list of files to download");
-		System.out.println("You can also leave this field blank if you wish to download the entire directory");
-		String stringDirectory = scanner.nextLine();
-		if (stringDirectory.isBlank()) {
-			return null;
-		}
-		File fileDirectory = new File(stringDirectory);
-		if (!fileDirectory.canRead()) {
-			System.out.println("Cannot read from directory: " + fileDirectory);
-			System.out.println("Please try again");
-			return getPathToListOfFilesToDownload();
-		}
-		return fileDirectory;
-	}
-
-	public File execute() {
-		try {
-			List<String> listOfRepos = FileUtils.getListOfStringsFromFile(pathToRepoNameFile);
-			for (String repo : listOfRepos) {
-				URLbyComponents url = null;
-				url = new URLbyComponents(host, username, repo, subdirectory, branch);
-
-				System.out.println(
-						"Downloading files from: " + (url.getPath().equals("") ? url.getBareURL() : url.getFullURL())
-								+ " to directory: " + directoryToDownloadTo);
-				GitHandlerAbstract gitHandler = new GitHandlerMiddleManager().setURL(url).setDir(directoryToDownloadTo)
-						.setDate(timestamp);
-				if (oauthToken != null) {
-					gitHandler.setCredentials(oauthToken);
-				}
-				if (pathToListOfFilesToDownload != null) {
-					List<String> filesToDownload = FileUtils.getListOfStringsFromFile(pathToListOfFilesToDownload);
-					gitHandler.setFilesToDownload(filesToDownload);
-				}
-				gitHandler.execute();
-				Enviroment.addStudentDirectory(new File(directoryToDownloadTo, repo));
-			}
-
-			Enviroment.setRootWorkingStudentFileDir(directoryToDownloadTo);
-			return directoryToDownloadTo;
-		} catch (IOException e) {
-			throw new IllegalStateException(e);
-		}
-	}
-
-	public void setUnsetFields(boolean getOptionalFields) {
-		if (username == null) {
-			username = getUsername();
-		}
 		if (pathToRepoNameFile == null) {
-			pathToRepoNameFile = getPathToRepoNameFile();
+			throw new IllegalArgumentException("Repo name file not set");
 		}
+
+		if (!pathToRepoNameFile.canRead()) {
+			throw new IllegalArgumentException("Cannot read from repo name file: " + pathToRepoNameFile);
+		}
+
+		if (pathToListOfFilesToDownload != null && !pathToListOfFilesToDownload.canRead()) {
+			throw new IllegalArgumentException("Cannot read from repo name file: " + pathToRepoNameFile);
+		}
+
+		if (username == null) {
+			throw new IllegalStateException("Git Username is not set");
+		}
+
+		if (pathToRepoNameFile == null) {
+			throw new IllegalStateException("Git Repo Path is not set");
+		}
+
 		if (branch == null) {
-			branch = getBranch();
+			throw new IllegalStateException("Git Branch is not set");
 		}
-		if (directoryToDownloadTo == null) {
-			directoryToDownloadTo = getDirectoryToDownloadTo();
-		}
-		if (getOptionalFields) {
-			if (host == null) {
-				host = getHost();
-			}
-			if (subdirectory == null) {
-				subdirectory = getSubdirectory();
-			}
-			if (oauthToken == null) {
-				oauthToken = getOauthToken();
-			}
-			if (timestamp == null) {
-				timestamp = getTimeStamp();
-			}
-			if (pathToListOfFilesToDownload == null) {
-				pathToListOfFilesToDownload = getPathToListOfFilesToDownload();
-			}
-		}
-	}
 
-	public void setFields(String[] params) {
-		for (int i = 0; i < params.length; i++) {
-			switch (params[i].toLowerCase()) {
-			case "-h":
-			case "-host":
-				host = params[++i];
-				break;
-
-			case "-u":
-			case "-user":
-			case "-username":
-				username = params[++i];
-				break;
-
-			case "-r":
-			case "-repo":
-				File pathToRepoName = new File(params[++i]);
-				if (!pathToRepoName.canRead()) {
-					throw new IllegalStateException("Cannot read from file: " + pathToRepoName);
-				}
-				pathToRepoNameFile = pathToRepoName;
-				break;
-
-			case "-sd":
-			case "-subdirectory":
-				subdirectory = params[++i];
-				break;
-
-			case "-b":
-			case "-branch":
-				branch = params[++i];
-				break;
-
-			case "-o":
-			case "-oauth":
-			case "-oauthtoken":
-				oauthToken = params[++i].toCharArray();
-				break;
-
-			case "-d":
-			case "-dir":
-			case "-directory":
-				directoryToDownloadTo = new File(params[++i]);
-				break;
-
-			case "-t":
-			case "-time":
-			case "-timestamp":
-				timestamp = LocalDateTime.parse(params[++i]);
-				break;
-			case "-files":
-			case "-f":
-				pathToListOfFilesToDownload = new File(params[++i]);
-				break;
-			default:
-				throw new IllegalArgumentException(params[i] + " is not a valid flag");
-			}
-		}
 		if (directoryToDownloadTo == null) {
 			try {
 				directoryToDownloadTo = Files.createTempDirectory("MOSSAutoSubmit_GitFiles").toFile();
@@ -276,11 +72,49 @@ public class GitRunner {
 				throw new IllegalStateException(
 						"Cannot create temp directory for you, please specificy a valid directory and try again");
 			}
-		} else {
-			if (directoryToDownloadTo.exists() && FileUtils.getNumberOfFiles(directoryToDownloadTo) != 0) {
-				throw new IllegalStateException("Directory must be empty " + directoryToDownloadTo);
-			}
+		}
+		if (!directoryToDownloadTo.canWrite()) {
+			throw new IllegalArgumentException("Cannot write to download directory: " + pathToRepoNameFile);
 		}
 	}
 
+	public File execute() throws IOException {
+		List<String> filesToDownload = null;
+		if (pathToListOfFilesToDownload != null) {
+			filesToDownload = FileUtils.getListOfStringsFromFile(pathToListOfFilesToDownload);
+		}
+		List<String> listOfRepos = FileUtils.getListOfStringsFromFile(pathToRepoNameFile);
+		boolean success = false;
+		for (String repo : listOfRepos) {
+
+			try {
+				URLbyComponents url = new URLbyComponents(gitHost, username, repo, subdirectory, branch);
+
+				System.out.println(
+						"Downloading files from: " + (url.getPath().equals("") ? url.getBareURL() : url.getFullURL())
+								+ " to directory: " + directoryToDownloadTo);
+				GitHandlerAbstract gitHandler = new GitHandlerMiddleManager().setURL(url).setDir(directoryToDownloadTo)
+						.setDate(timestamp);
+				if (oauthToken.length > 0) {
+					gitHandler.setCredentials(oauthToken);
+				}
+				if (filesToDownload != null) {
+					gitHandler.setFilesToDownload(filesToDownload);
+
+				}
+				gitHandler.execute();
+				success = true;
+			} catch (IOException e) {
+				System.err.println("Failed to download repo: " + repo);
+				System.err.println("Operation failed with the message: " + e.getMessage());
+			}
+
+		}
+
+		if (!success) {
+			throw new IOException("0 Git downloads completed succesfully - check error log");
+		}
+
+		return directoryToDownloadTo;
+	}
 }
