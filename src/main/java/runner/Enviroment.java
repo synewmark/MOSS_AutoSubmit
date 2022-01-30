ee1f638476c0ac9a3bef1e98c1cdaca2c1fc95e9
Shmuel Newmark <snewmark@mail.yu.edu>
2022-01-30 17:46:52 -0500

    Makes use of Google Options instead of manual switch case parsing

--- src/main/java/runner/Enviroment.java
+++ src/main/java/runner/Enviroment.java
@@ -1,43 +1,97 @@
 package runner;
 
 import java.io.File;
-import java.util.Collections;
-import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
 
-public class Enviroment {
-	private static String workingLanguage;
-	private static File rootWorkingStudentFileDir;
-	private static SortedSet<File> studentDirectories = new TreeSet<>();
-	private static Set<File> unmodifiableStudentDirectories = Collections.unmodifiableSet(studentDirectories);
+import com.google.devtools.common.options.Converter;
+import com.google.devtools.common.options.Option;
+import com.google.devtools.common.options.OptionsBase;
+import com.google.devtools.common.options.OptionsParsingException;
 
-	public static String getWorkingLanguage() {
-		return workingLanguage;
+public class Enviroment extends OptionsBase {
+	@Option(name = "git", abbrev = 'g', help = "Github Request Oauth Token", category = "git", defaultValue = "", converter = CharArrayConverter.class)
+	public char[] gitAPI;
+	@Option(name = "username", abbrev = 'u', help = "Github Username", category = "git", defaultValue = "")
+	public String username;
+	@Option(name = "repo", abbrev = 'r', help = "Path to repo text file", category = "git", defaultValue = "", converter = FileConverter.class)
+	public File repoPath;
+	@Option(name = "branch", abbrev = 'b', help = "branch of GitHub repo", category = "git", defaultValue = "")
+	public String branch;
+	@Option(name = "subdirectory", help = "Explicit subdirectory of files to download", category = "git", defaultValue = "")
+	public String subdirectory;
+	@Option(name = "files", help = "Text file containing explicit files to download", category = "git", defaultValue = "", converter = FileConverter.class)
+	public File filesToDownload;
+	@Option(name = "timestamp", help = "ISO 8601 timestamp in the format: YYYY-MM-DDTHH:MM:SSZ", category = "git", defaultValue = "", converter = LDTConverter.class)
+	public LocalDateTime timestamp;
+
+	@Option(name = "moss", abbrev = 'm', help = "MOSS Request User ID", category = "MOSS", defaultValue = "-1")
+	public long mossID;
+	@Option(name = "basefiles", help = "Directory of baseFiles for MOSS", category = "MOSS", defaultValue = "", converter = FileConverter.class)
+	public File baseFiles;
+
+	@Option(name = "codequiry", abbrev = 'c', help = "Codequiry Request API key", category = "Codequiry", defaultValue = "")
+	public String codequiryAPI;
+	@Option(name = "name", help = "Codequiry custom Request name", category = "Codequiry", defaultValue = "")
+	public String codequiryRequestName;
+
+	@Option(name = "language", abbrev = 'l', help = "Language of student code", category = "MOSS & Codequiry", defaultValue = "")
+	public String language;
+
+	@Option(name = "directory", abbrev = 'd', help = "Working directory to download and upload files from", category = "Git & MOSS & Codequiry", defaultValue = "", converter = FileConverter.class)
+	public File directory;
+
+	public static class FileConverter implements Converter<File> {
+		@Override
+		public File convert(String input) throws OptionsParsingException {
+			if (input == null || input.isBlank()) {
+				return null;
+			}
+			return new File(input);
+		}
+
+		@Override
+		public String getTypeDescription() {
+			return "File from String";
+		}
 	}
 
-	public static void setWorkingLanguage(String workingLanguage) {
-		Enviroment.workingLanguage = workingLanguage;
+	public static class LDTConverter implements Converter<LocalDateTime> {
+		@Override
+		public LocalDateTime convert(String input) throws OptionsParsingException {
+			if (input == null || input.isBlank()) {
+				return LocalDateTime.now();
+			}
+			try {
+				return LocalDateTime.parse(input);
+			} catch (DateTimeParseException e) {
+				throw new OptionsParsingException(
+						"Time stamp must be in the format: YYYY-MM-DDTHH:MM:SSZ " + e.getMessage(), e);
+			}
+		}
+
+		@Override
+		public String getTypeDescription() {
+			return "Timestamp from String";
+		}
 	}
 
-	public static File getRootWorkingStudentFileDir() {
-		return rootWorkingStudentFileDir;
+	public static class CharArrayConverter implements Converter<char[]> {
+
+		@Override
+		public char[] convert(String input) throws OptionsParsingException {
+			if (input == null) {
+				return null;
+			}
+			if (input.isBlank()) {
+				return new char[0];
+			}
+			return input.toCharArray();
+		}
+
+		@Override
+		public String getTypeDescription() {
+			return "Char array from String";
+		}
 	}
-
-	public static void setRootWorkingStudentFileDir(File rootWorkingStudentFileDir) {
-		Enviroment.rootWorkingStudentFileDir = rootWorkingStudentFileDir;
-	}
-
-	public static boolean addStudentDirectory(File directory) {
-		return studentDirectories.add(directory);
-	}
-
-	public static Set<File> getStudentDirectories() {
-		return unmodifiableStudentDirectories;
-	}
-
-	private Enviroment() {
-
-	}
-
 }
