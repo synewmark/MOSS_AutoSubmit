a9f01ae6764fcd2c024d4fdccd3005f9d8e0871f
Shmuel Newmark <snewmark@mail.yu.edu>
2022-01-31 18:34:01 -0500

    Tests no longer test for existence of MossRequestResults file
    File no longer has a stable name and is up a directory structure

--- src/test/java/runner/MainCommandLineTests.java
+++ src/test/java/runner/MainCommandLineTests.java
@@ -1,7 +1,6 @@
 package runner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
@@ -34,21 +33,21 @@ public class MainCommandLineTests {
 
 	@Test
 	public void testExplicitFilesTempDir() {
-		String args[] = ("-g   -u synewmark-resources -r " + repos + " --files " + listOfFiles1Through4
+		String args[] = ("-g  -u synewmark-resources -r " + repos + " --files " + listOfFiles1Through4
 				+ " -b main -m 884640278 -l java").split(" ");
+		args[1] = " ";
 		MainCommandLine.main(args);
-		assertEquals(9, FileUtils.getNumberOfFiles(MainCommandLine.enviroment.directory));
-		assertTrue(new File(MainCommandLine.enviroment.directory, "MossRequestResults.htm").exists());
+		assertEquals(8, FileUtils.getNumberOfFiles(MainCommandLine.enviroment.directory));
 	}
 
 	@Test
 	public void testAllFilesTestDir() {
 		testDir.mkdir();
-		String args[] = ("-g   -u synewmark-resources -r " + repos + " -d " + testDir + " -b main -m 884640278 -l java")
+		String args[] = ("-g  -u synewmark-resources -r " + repos + " -d " + testDir + " -b main -m 884640278 -l java")
 				.split(" ");
+		args[1] = " ";
 		MainCommandLine.main(args);
-		assertEquals(21, FileUtils.getNumberOfFiles(MainCommandLine.enviroment.directory));
-		assertTrue(new File(MainCommandLine.enviroment.directory, "MossRequestResults.htm").exists());
+		assertEquals(20, FileUtils.getNumberOfFiles(MainCommandLine.enviroment.directory));
 	}
 
 	@Test
@@ -56,36 +55,38 @@ public class MainCommandLineTests {
 		testDir.mkdir();
 		String args[] = ("-g  -u synewmark-resources -r " + repos + " -d " + testDir + " --files "
 				+ listOfFiles1Through4 + " -b main -m 884640278 -l java").split(" ");
+		args[1] = " ";
 		MainCommandLine.main(args);
-		assertEquals(9, FileUtils.getNumberOfFiles(MainCommandLine.enviroment.directory));
-		assertTrue(new File(MainCommandLine.enviroment.directory, "MossRequestResults.htm").exists());
+		assertEquals(8, FileUtils.getNumberOfFiles(MainCommandLine.enviroment.directory));
 	}
 
 	@Test
 	public void testGitAndMossSeperately() {
-		String args1[] = ("-g   -u synewmark-resources -r " + repos + " -b main -m 884640278 -l java").split(" ");
+		String args1[] = ("-g  -u synewmark-resources -r " + repos + " -b main -m 884640278 -l java").split(" ");
+		args1[1] = " ";
 		MainCommandLine.main(args1);
 		File sfd = MainCommandLine.enviroment.directory;
 		MainCommandLine.enviroment.directory = null;
 		String args2[] = ("-m 884640278 -l java -d " + sfd).split(" ");
 		MainCommandLine.main(args2);
-		assertEquals(21, FileUtils.getNumberOfFiles(MainCommandLine.enviroment.directory));
-		assertTrue(new File(MainCommandLine.enviroment.directory, "MossRequestResults.htm").exists());
+		assertEquals(20, FileUtils.getNumberOfFiles(MainCommandLine.enviroment.directory));
 	}
 
 	@Test
 	public void testGitTimeStampNow() {
-		String args1[] = ("-g   -u synewmark-resources -b main -r " + students + " --timestamp " + LocalDateTime.now()
+		String args1[] = ("-g  -u synewmark-resources -b main -r " + students + " --timestamp " + LocalDateTime.now()
 				+ " --subdirectory " + "DataStructures-Spring2021-Project/stage2").split(" ");
+		args1[1] = " ";
 		MainCommandLine.main(args1);
 		assertEquals(34, FileUtils.getNumberOfFiles(MainCommandLine.enviroment.directory));
 	}
 
 	@Test
 	public void testGitTimeStampNowJan30() {
-		String args1[] = ("-g   -u synewmark-resources -b main -r " + students
+		String args1[] = ("-g  -u synewmark-resources -b main -r " + students
 				+ " --timestamp 2022-01-30T00:00 --subdirectory " + "DataStructures-Spring2021-Project/stage1")
 						.split(" ");
+		args1[1] = " ";
 		MainCommandLine.main(args1);
 		assertEquals(30, FileUtils.getNumberOfFiles(MainCommandLine.enviroment.directory));
 	}
